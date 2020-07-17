552220bc361cda33dea89be7122fa58228754d98
Michal Kechner <michal.kechner@columbusglobal.com>
2020-07-17 13:35:47 +0200

    chore: add swedish language & currency support to i18n module

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -101,6 +101,7 @@ export const ENVIRONMENT_DEFAULTS: Environment = {
   defaultDeviceType: 'mobile',
   locales: [
     { lang: 'en_US', currency: 'USD', value: 'en', displayName: 'English', displayLong: 'English (United States)' },
+    { lang: 'sv_SE', currency: 'SEK', value: 'se', displayName: 'Swedish', displayLong: 'Swedish (Sweden)' },
     { lang: 'de_DE', currency: 'EUR', value: 'de', displayName: 'German', displayLong: 'German (Germany)' },
     { lang: 'fr_FR', currency: 'EUR', value: 'fr', displayName: 'French', displayLong: 'French (France)' },
   ],
