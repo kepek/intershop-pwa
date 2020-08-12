f7d72f8f7b7e838c251e33f07a679b36acce9cae
USchellbach <u.schellbach@intershop.de>
2020-08-12 16:47:02 +0200

    merge comment removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,7 @@ export const environment: Environment = {
   ...ENVIRONMENT_DEFAULTS,
 
   /* INTERSHOP COMMERCE MANAGEMENT REST API CONFIGURATION */
-<<<<<<< HEAD
-  icmBaseURL: 'https://intershoppwa.azurewebsites.net',
-  theme: 'camfil',
-=======
   icmBaseURL: 'https://intershoppwa-b2b.azurewebsites.net',
 
   production: true,
->>>>>>> 15275ac7d2e8619bbc5cd8577d167adc20bdb62f
 };
