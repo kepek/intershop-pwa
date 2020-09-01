af535688050a379bbae0f5950f7ea97553ffcd59
Michal Kechner <michal.kechner@columbusglobal.com>
2020-09-01 16:51:36 +0200

    fix: update currentFilter$ observer to do not break filters are undefined

--- src/app/core/facades/shopping.facade.ts
+++ src/app/core/facades/shopping.facade.ts
@@ -166,7 +166,9 @@ export class ShoppingFacade {
     return this.store.pipe(
       select(getAvailableFilter),
       whenTruthy(),
-      map(x => (withCategoryFilter ? x : { ...x, filter: x.filter.filter(f => f.id !== 'CategoryUUIDLevelMulti') }))
+      map(x =>
+        withCategoryFilter ? x : { ...x, filter: [...x?.filter].filter(f => f.id !== 'CategoryUUIDLevelMulti') }
+      )
     );
   }
 
