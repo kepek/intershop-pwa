a95bb0ccb5e703da7bf90a0d5dbf1496869a5184
Michal Kechner <michal.kechner@columbusglobal.com>
2020-09-10 18:27:39 +0200

    fix: resolve broken tests in 'camfil-applied-filter' component

--- src/app/shared/components/filter/camfil-filter-applied/camfil-filter-applied.component.spec.ts
+++ src/app/shared/components/filter/camfil-filter-applied/camfil-filter-applied.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { instance, mock } from 'ts-mockito';
+
+import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 
 import { CamfilFilterAppliedComponent } from './camfil-filter-applied.component';
 
@@ -10,6 +14,8 @@ describe('Camfil Filter Applied Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CamfilFilterAppliedComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: ShoppingFacade, useFactory: () => instance(mock(ShoppingFacade)) }],
     }).compileComponents();
   }));
 
