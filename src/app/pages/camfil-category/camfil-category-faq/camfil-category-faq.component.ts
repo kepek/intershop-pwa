8da81a5d700aface875bdc1307917f2f53f42df8
beovulf <pcichon88@gmail.com>
2020-09-22 13:43:36 +0200

    CAM-499 faq
    Fixes after PR

---
+++ src/app/pages/camfil-category/camfil-category-faq/camfil-category-faq.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'camfil-category-faq',
+  templateUrl: './camfil-category-faq.component.html',
+  styleUrls: ['./camfil-category-faq.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CamfilCategoryFaqComponent {}
