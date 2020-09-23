1afa7cd49824d22796189021861c5119998cbf69
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2020-09-23 09:41:44 +0200

    camcards-init: mat-table with sorting + acordion

--- src/app/pages/demo/demo-page.component.ts
+++ src/app/pages/demo/demo-page.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
@@ -16,28 +17,36 @@ import { map, startWith, take, takeUntil } from 'rxjs/operators';
 import { DemoBottomSheetComponent } from './demo-bottom-sheet/demo-bottom-sheet.component';
 import { DemoDialogComponent } from './demo-dialog/demo-dialog.component';
 
+export interface Fruit {
+  name: string;
+}
+
+export interface SubPeriodicElement {
+  header: string;
+  insideData: string[];
+}
+
 export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
   symbol: string;
+  other: SubPeriodicElement[];
 }
 
-export interface Fruit {
-  name: string;
-}
+const ELEMENT_DATA_OTHER: SubPeriodicElement[] = [
+  { header: 'Building 1', insideData: ['product sku - 12345', 'product sku - 12645'] },
+  { header: 'Building 2', insideData: ['product sku - 98265', 'product sku - 761'] },
+];
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 1, name: 'CamCard Hydrogen', weight: 1.0079, symbol: 'H', other: ELEMENT_DATA_OTHER },
+  { position: 2, name: 'CamCard test', weight: 4.0026, symbol: 'He', other: ELEMENT_DATA_OTHER },
+  { position: 3, name: 'CamCard Lithium', weight: 6.941, symbol: 'Li', other: ELEMENT_DATA_OTHER },
+  { position: 4, name: 'CamCard Beryllium', weight: 9.0122, symbol: 'Be', other: ELEMENT_DATA_OTHER },
+  { position: 5, name: 'CamCard Boron', weight: 10.811, symbol: 'B', other: ELEMENT_DATA_OTHER },
+  { position: 6, name: 'CamCard Carbon', weight: 12.0107, symbol: 'C', other: ELEMENT_DATA_OTHER },
+  { position: 7, name: 'CamCard Nitrogen', weight: 14.0067, symbol: 'N', other: ELEMENT_DATA_OTHER },
 ];
 
 // tslint:disable-next-line:component-creation-test
@@ -53,6 +62,13 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './demo-page.component.html',
   styleUrls: ['./demo-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DemoPageComponent implements AfterViewInit, OnInit, OnDestroy {
   private destroy$ = new Subject();
@@ -136,8 +152,11 @@ export class DemoPageComponent implements AfterViewInit, OnInit, OnDestroy {
     { name: 'Warn', color: 'warn' },
   ];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  /** table */
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  expandedElement: PeriodicElement | null;
 
   visibleFruits = true;
   selectableFruits = true;
