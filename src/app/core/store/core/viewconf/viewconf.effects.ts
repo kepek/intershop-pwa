2bd8e8a1768ae332a1a09211bb784b20a263474a
Michal Kechner <michal.kechner@columbusglobal.com>
2020-10-09 17:38:29 +0200

    test: add missing operators

--- src/app/core/store/core/viewconf/viewconf.effects.ts
+++ src/app/core/store/core/viewconf/viewconf.effects.ts
@@ -4,13 +4,23 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { routerNavigatedAction, routerRequestAction } from '@ngrx/router-store';
 import { Store, select } from '@ngrx/store';
 import { defer, fromEvent, iif } from 'rxjs';
-import { bufferToggle, concatMap, delay, distinctUntilChanged, filter, first, map } from 'rxjs/operators';
+import {
+  bufferToggle,
+  concatMap,
+  delay,
+  distinctUntilChanged,
+  filter,
+  first,
+  map,
+  withLatestFrom,
+} from 'rxjs/operators';
 
 import { BreadcrumbItem } from 'ish-core/models/breadcrumb-item/breadcrumb-item.interface';
 import { selectRouteData } from 'ish-core/store/core/router';
+import { getScroll } from 'ish-core/store/core/viewconf/viewconf.selectors';
 import { whenTruthy } from 'ish-core/utils/operators';
 
-import { setBreadcrumbData, setStickyHeader } from './viewconf.actions';
+import { setBreadcrumbData, setScroll, setStickyHeader } from './viewconf.actions';
 
 @Injectable()
 export class ViewconfEffects {
