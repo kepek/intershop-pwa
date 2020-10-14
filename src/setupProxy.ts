8ce1772f6d3b2cbba330fd63bb7676fd4db27699
Michal Kechner <michal.kechner@columbusglobal.com>
2020-10-14 15:09:56 +0200

    fix: make sure to do not proxy when env variable does not exist

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,6 +1,9 @@
-const https = require('https');
-
-const http = require('http');
+const getHttpAgent = endpoint => {
+  const https = require('https');
+  const http = require('http');
+  const httpProtocol = new URL(endpoint).protocol.slice(0, -1);
+  return httpProtocol === 'https' ? new https.Agent({ rejectUnauthorized: false }) : new http.Agent();
+};
 
 const PROXY_ICM = process.env.PROXY_ICM;
 
@@ -10,12 +13,7 @@ const isUrl = PROXY_ICM && PROXY_ICM.startsWith('http');
 
 const useProxy = isUrl;
 
-const getHttpAgent = endpoint => {
-  const httpProtocol = new URL(endpoint).protocol.slice(0, -1);
-  return httpProtocol === 'https' ? new https.Agent({ rejectUnauthorized: false }) : new http.Agent();
-};
-
-if (!isUrl) {
+if (PROXY_ICM && !isUrl) {
   console.error('PROXY_ICM is not url');
   process.exit(1);
 }
