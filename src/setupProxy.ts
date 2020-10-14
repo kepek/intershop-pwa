9610cd0664ab564cd977503532b52f2393816f7a
Michal Kechner <michal.kechner@columbusglobal.com>
2020-10-14 10:27:23 +0200

    feat: add setup for reverse proxy

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,10 +1,33 @@
-const PROXY_ICM = process.env.PROXY_ICM || 'https://intershoppwa.azurewebsites.net';
+const https = require('https');
 
-module.exports = {
-  '/INTERSHOP': {
-    target: PROXY_ICM,
-    secure: false,
-    changeOrigin: true,
-    logLevel: 'debug',
-  },
+const http = require('http');
+
+const PROXY_ICM = process.env.PROXY_ICM;
+
+const TRUST_ICM = Boolean(Number(JSON.parse(process.env.TRUST_ICM))) || true;
+
+const isUrl = PROXY_ICM && PROXY_ICM.startsWith('http');
+
+const useProxy = isUrl;
+
+const getHttpAgent = endpoint => {
+  const httpProtocol = new URL(endpoint).protocol.slice(0, -1);
+  return httpProtocol === 'https' ? new https.Agent({ rejectUnauthorized: false }) : new http.Agent();
 };
+
+if (!isUrl) {
+  console.error('PROXY_ICM is not url');
+  process.exit(1);
+}
+
+module.exports = !useProxy
+  ? {}
+  : {
+      '/INTERSHOP/*': {
+        target: PROXY_ICM,
+        secure: TRUST_ICM,
+        changeOrigin: true,
+        logLevel: 'debug',
+        agent: getHttpAgent(PROXY_ICM),
+      },
+    };
