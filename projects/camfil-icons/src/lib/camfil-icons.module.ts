cf5d33e6f4d378311f0f0c595918c3056db74965
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2020-10-20 14:14:44 +0200

    CAM-439 - As a User I want to be displayed talk bubble on article rows, given a comment already exist, on the CamCard page

--- projects/camfil-icons/src/lib/camfil-icons.module.ts
+++ projects/camfil-icons/src/lib/camfil-icons.module.ts
@@ -7,8 +7,16 @@ import { completeIconSet } from '../exports';
 @NgModule({})
 export class CamfilIconsModule {
   constructor(private domSanitizer: DomSanitizer, public matIconRegistry: MatIconRegistry) {
-    completeIconSet.forEach(icon => {
-      this.matIconRegistry.addSvgIconLiteral(icon.name, this.domSanitizer.bypassSecurityTrustHtml(icon.data));
+    completeIconSet.forEach(({ name, data }) => {
+      let options = {};
+      const start = data.indexOf('viewBox="');
+
+      if (start > -1) {
+        const end = data.indexOf('"', start + 9);
+        const val = data.slice(start + 9, end);
+        options = { viewBox: val };
+      }
+      this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(data), options);
     });
   }
 }
