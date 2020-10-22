2dd71a59c67548ba7e02dc9f1b01b92620f1d035
beovulf <pcichon88@gmail.com>
2020-10-22 12:37:18 +0200

    update-camcard-header

--- src/app/extensions/cam-cards/store/cam-card/cam-card.effects.ts
+++ src/app/extensions/cam-cards/store/cam-card/cam-card.effects.ts
@@ -210,8 +210,6 @@ export class CamCardEffects {
     )
   );
 
-  updateCamCardSuccess$ = createEffect(() => this.actions$.pipe(ofType(updateCamCardSuccess), mapTo(loadCamCards())));
-
   addProductToCamCard$ = createEffect(() =>
     this.actions$.pipe(
       ofType(addProductToCamCard),
