b7820dad084e89fe6d37f35a6039d7fcf7f5866c
Michal Kechner <michal.kechner@columbusglobal.com>
2020-10-27 13:40:51 +0100

    fix: rename component in example usage

--- src/app/extensions/captcha/exports/lazy-captcha/lazy-captcha.component.ts
+++ src/app/extensions/captcha/exports/lazy-captcha/lazy-captcha.component.ts
@@ -29,7 +29,7 @@ import { CaptchaFacade, CaptchaTopic } from '../../facades/captcha.facade';
  * The parent form supplied must have controls for 'captcha' and 'captchaAction'
  *
  * @example
- * <camfil-lazy-captcha [form]="form" cssClass="offset-md-2 col-md-8" topic="contactUs"></camfil-lazy-captcha>
+ * <ish-lazy-captcha [form]="form" cssClass="offset-md-2 col-md-8" topic="contactUs"></camfil-lazy-captcha>
  */
 @Component({
   selector: 'ish-lazy-captcha',
