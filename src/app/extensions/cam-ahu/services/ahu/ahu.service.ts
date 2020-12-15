595cdb80b26c90f6f467e1b86f47a940b6b9f8e4
Michal Kechner <michal.kechner@columbusglobal.com>
2020-12-15 01:21:33 +0100

    fix: cr due to ther latest icc/ahu api changes

--- src/app/extensions/cam-ahu/services/ahu/ahu.service.ts
+++ src/app/extensions/cam-ahu/services/ahu/ahu.service.ts
@@ -1,6 +1,5 @@
-import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, OperatorFunction, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { ApiService as IccApiService } from '../../../cam-icc/services/api/api.service';
@@ -10,6 +9,10 @@ import { Manufacturer } from '../../models/manufacturer/manufacturer.model';
 import { UnitData } from '../../models/unit/unit.interface';
 import { UnitMapper } from '../../models/unit/unit.mapper';
 import { Unit } from '../../models/unit/unit.model';
+
+export function unpackHeap<T>(): OperatorFunction<[], T[]> {
+  return map(data => (!!data && !!data.length ? data : []));
+}
 
 @Injectable({ providedIn: 'root' })
 export class AhuService {
@@ -21,31 +24,26 @@ export class AhuService {
    * @param market              Specify market or get all results matching the query
    */
   getManufacturers(market?: string): Observable<Manufacturer[]> {
-    const params = new HttpParams().set('market', market);
+    const requestBody = { market };
 
-    return this.iccApiService
-      .get<ManufacturerData[]>('ahu/manufacturer', {
-        params,
-      })
-      .pipe(map(data => ManufacturerMapper.fromListData(data)));
+    return this.iccApiService.post<ManufacturerData[]>('ahu/manufacturer', requestBody).pipe(
+      unpackHeap<ManufacturerData>(),
+      map(data => ManufacturerMapper.fromListData(data))
+    );
   }
 
-  getManufacturer(manufacturerId: string, market?: string): Observable<Manufacturer> {
-    const params = new HttpParams().set('id', manufacturerId).set('market', market);
+  getManufacturer(id: string, market?: string): Observable<Manufacturer> {
+    const requestBody = { id, market };
 
-    return this.iccApiService
-      .get<ManufacturerData>('ahu/manufacturer', {
-        params,
+    return this.iccApiService.post<ManufacturerData>('ahu/manufacturer', requestBody).pipe(
+      map(data => {
+        if (Array.isArray(data)) {
+          return ManufacturerMapper.fromListData(data).find(m => m.id === id);
+        }
+
+        return ManufacturerMapper.fromData(data);
       })
-      .pipe(
-        map(data => {
-          if (Array.isArray(data)) {
-            return ManufacturerMapper.fromListData(data).find(m => m.id === manufacturerId);
-          }
-
-          return ManufacturerMapper.fromData(data);
-        })
-      );
+    );
   }
 
   /**
@@ -58,13 +56,15 @@ export class AhuService {
       return throwError('getUnits() called without manufacturerId');
     }
 
-    const params = new HttpParams().set('ManufacturerId', manufacturerId).set('Market', market);
+    const requestBody = {
+      ManufacturerId: manufacturerId,
+      Market: market,
+    };
 
-    return this.iccApiService
-      .get<UnitData[]>('ahu/unit', {
-        params,
-      })
-      .pipe(map(data => UnitMapper.fromListData(data)));
+    return this.iccApiService.post<UnitData[]>('ahu/unit', requestBody).pipe(
+      unpackHeap<UnitData>(),
+      map(data => UnitMapper.fromListData(data))
+    );
   }
 
   /**
@@ -79,20 +79,20 @@ export class AhuService {
       return throwError('getUnits() called without unitId');
     }
 
-    const params = new HttpParams().set('UnitId', unitId).set('Market', market).set('SlotId', slotId);
+    const requestBody = {
+      UnitId: unitId,
+      Market: market,
+      SlotId: slotId,
+    };
 
-    return this.iccApiService
-      .get<UnitData>('ahu/unit', {
-        params,
+    return this.iccApiService.post<UnitData>('ahu/unit', requestBody).pipe(
+      map(data => {
+        if (Array.isArray(data)) {
+          return UnitMapper.fromListData(data).find(u => u?.ahu?.id === unitId);
+        }
+
+        return UnitMapper.fromData(data);
       })
-      .pipe(
-        map(data => {
-          if (Array.isArray(data)) {
-            return UnitMapper.fromListData(data).find(u => u?.ahu?.id === unitId);
-          }
-
-          return UnitMapper.fromData(data);
-        })
-      );
+    );
   }
 }
