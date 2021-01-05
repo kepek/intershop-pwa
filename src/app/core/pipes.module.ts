2b06eec41480bdb3ed217e3b8e747241f4862526
Michal Kechner <michal.kechner@columbusglobal.com>
2021-01-05 15:35:02 +0100

    [post-upgrade] rollback account navigation - disable camcards & orders history temporarily

--- src/app/core/pipes.module.ts
+++ src/app/core/pipes.module.ts
@@ -5,9 +5,11 @@ import { PricePipe } from './models/price/price.pipe';
 import { CamfilDatePipe } from './pipes/camfil-date.pipe';
 import { CamfilDimensionPipe } from './pipes/camfil-dimension.pipe';
 import { DatePipe } from './pipes/date.pipe';
+import { FeatureTogglePipe } from './pipes/feature-toggle.pipe';
 import { HighlightPipe } from './pipes/highlight.pipe';
 import { MakeHrefPipe } from './pipes/make-href.pipe';
 import { SanitizePipe } from './pipes/sanitize.pipe';
+import { ServerSettingPipe } from './pipes/server-setting.pipe';
 import { CategoryRoutePipe } from './routing/category/category-route.pipe';
 import { ProductRoutePipe } from './routing/product/product-route.pipe';
 
@@ -17,11 +19,13 @@ const pipes = [
   CamfilDimensionPipe,
   CategoryRoutePipe,
   DatePipe,
+  FeatureTogglePipe,
   HighlightPipe,
   MakeHrefPipe,
   PricePipe,
   ProductRoutePipe,
   SanitizePipe,
+  ServerSettingPipe,
 ];
 
 @NgModule({
--- src/app/pages/account/account-page.module.ts
+++ src/app/pages/account/account-page.module.ts
@@ -25,7 +25,7 @@ const accountPageRoutes: Routes = [
         path: 'orders',
         data: { breadcrumbData: [{ key: 'account.order_history.link' }] },
         loadChildren: () =>
-          import('../camfil-account-order-history/camfil-account-order-history-page.module').then(
+          import('../account-order-history/account-order-history-page.module').then(
             m => m.AccountOrderHistoryPageModule
           ),
       },
@@ -36,12 +36,10 @@ const accountPageRoutes: Routes = [
           import('../account-payment/account-payment-page.module').then(m => m.AccountPaymentPageModule),
       },
       {
-        path: '',
+        path: 'profile',
         data: { breadcrumbData: [{ key: 'account.profile.link' }] },
         loadChildren: () =>
-          import('../../extensions/cam-account/pages/account-profile/account-profile-page.module').then(
-            m => m.AccountProfilePageModule
-          ),
+          import('../account-profile/account-profile-page.module').then(m => m.AccountProfilePageModule),
       },
       {
         path: 'quotes',
@@ -49,11 +47,11 @@ const accountPageRoutes: Routes = [
         loadChildren: () =>
           import('../../extensions/quoting/pages/quote-list/quote-list-page.module').then(m => m.QuoteListPageModule),
       },
-      /*      {
+      {
         path: '',
         data: { breadcrumbData: [] },
         component: AccountOverviewPageModule.component,
-      },*/
+      },
       {
         path: 'wishlists',
         data: { breadcrumbData: [{ key: 'account.wishlists.breadcrumb_link' }] },
