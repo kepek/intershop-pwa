df23af6872100c8351b14c218d57ff75d3ac657f
beovulf <pcichon88@gmail.com>
2021-01-14 16:05:26 +0100

    checkout-order-and-product-count

--- src/app/pages/camfil-checkout/camfil-checkout-header/camfil-checkout-header.component.ts
+++ src/app/pages/camfil-checkout/camfil-checkout-header/camfil-checkout-header.component.ts
@@ -12,4 +12,8 @@ import { Bucket } from 'ish-core/models/basket/bucket.model';
 export class CamfilCheckoutHeaderComponent {
   @Input() basket: BasketView;
   @Input() buckets: Bucket[];
+
+  totalProductQuantity() {
+    return this.buckets.reduce((a, b) => a + b.lineItems.reduce((c, d) => c + d.quantity.value, 0), 0);
+  }
 }
