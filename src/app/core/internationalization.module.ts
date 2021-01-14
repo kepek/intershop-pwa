d695d22bd47b6b5ba18c59be87b819b1adc864f3
Michal Kechner <michal.kechner@columbusglobal.com>
2021-01-14 17:21:59 +0100

    fix: make sure to load/use sv for swedish locales [CAM-649]

--- src/app/core/internationalization.module.ts
+++ src/app/core/internationalization.module.ts
@@ -2,7 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
 import localeFr from '@angular/common/locales/fr';
-import localeSe from '@angular/common/locales/se';
+import localeSv from '@angular/common/locales/sv';
 import { Inject, LOCALE_ID, NgModule } from '@angular/core';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -26,7 +26,7 @@ export class InternationalizationModule {
   constructor(@Inject(LOCALE_ID) lang: string, translateService: TranslateService) {
     registerLocaleData(localeDe);
     registerLocaleData(localeFr);
-    registerLocaleData(localeSe);
+    registerLocaleData(localeSv);
 
     translateService.setDefaultLang(lang.replace(/\-/, '_'));
   }
