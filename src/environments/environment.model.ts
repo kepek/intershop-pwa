d3c6b57adf1613bbe38cdedcfdfe7321aaab0506
Michal Kechner <michal.kechner@columbusglobal.com>
2021-01-18 22:50:55 +0100

    chore: simplify multi-language config [CAM-649]

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -156,10 +156,11 @@ export const ENVIRONMENT_DEFAULTS: Environment = {
   defaultProductListingViewType: 'simple',
   defaultDeviceType: 'mobile',
   locales: [
-    { lang: 'en_GB', currency: 'USD', value: 'en', displayName: 'English', displayLong: 'English (Great Britain)' },
-    { lang: 'sv_SE', currency: 'SEK', value: 'sv', displayName: 'Swedish', displayLong: 'Swedish (Sweden)' },
+    { lang: 'en_GB', currency: 'GBP', value: 'gb', displayName: 'British English', displayLong: 'English (Great Britain)' },
+    { lang: 'sv_SE', currency: 'SEK', value: 'se', displayName: 'Swedish', displayLong: 'Swedish (Sweden)' },
     { lang: 'de_DE', currency: 'EUR', value: 'de', displayName: 'German', displayLong: 'German (Germany)' },
     { lang: 'fr_FR', currency: 'EUR', value: 'fr', displayName: 'French', displayLong: 'French (France)' },
+    { lang: 'en_US', currency: 'USD', value: 'us', displayName: 'American English ', displayLong: 'English (United States)' },
   ],
   cookieConsentOptions: {
     options: {
