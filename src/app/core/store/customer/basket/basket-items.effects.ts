59266cf95f2feb9e9366bf2c856e2132be9bfd4f
malgorzatatylek_columbus <malgorzata.tylek@columbusglobal.com>
2021-01-22 15:49:21 +0100

    chore: npm run format

--- src/app/core/store/customer/basket/basket-items.effects.ts
+++ src/app/core/store/customer/basket/basket-items.effects.ts
@@ -190,7 +190,7 @@ export class BasketItemsEffects {
                     basketExtension: payload.basketExtensions,
                   }),
                 ]
-              : [git saddProductToBucketAddressFail()]),
+              : [addProductToBucketAddressFail()]),
           mapErrorToAction(addProductToBucketFail)
         )
       )
