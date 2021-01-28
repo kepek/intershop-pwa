741c739cee26c6e8b1425ef06717c91595ec4f6c
Michal Kechner <michal.kechner@columbusglobal.com>
2021-01-28 11:25:08 +0100

    build: update env settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,5 @@ export const environment: Environment = {
   ...ENVIRONMENT_DEFAULTS,
 
   /* INTERSHOP COMMERCE MANAGEMENT REST API CONFIGURATION */
-  icmBaseURL: 'https://pwa-ish-demo.test.intershop.com',
+  icmBaseURL: 'https://int-api.shop.camfil.com/',
 };
