6bffe98a43588eee10c843b9e07fa8bb4b469446
Philipp Sauer <PSauer@intershop.com>
2021-02-02 18:22:58 +0100

    CAM-340: Product video on pdp

--- src/app/pages/camfil-product/camfil-product-images/camfil-product-images.component.ts
+++ src/app/pages/camfil-product/camfil-product-images/camfil-product-images.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 import { Product, ProductHelper } from 'ish-core/models/product/product.model';
 
@@ -17,22 +19,44 @@ import { Product, ProductHelper } from 'ish-core/models/product/product.model';
   styleUrls: ['./camfil-product-images.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CamfilProductImagesComponent {
+export class CamfilProductImagesComponent implements OnInit {
+  constructor(private sanitizer: DomSanitizer) {}
   /**
    * The product for which the images should be displayed
    */
   @Input() product: Product;
+  @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
+  @ViewChild('videoFrame') videoFrame: ElementRef;
+  activeSlide = '0';
+  videoUrl: string;
+  secureVideoUrl: SafeResourceUrl;
+  secureVideoThumbnailUrl: SafeResourceUrl;
 
-  activeSlide = 0;
-
+  getImageCdnUrl = ProductHelper.getImageCdnUrl;
   getImageViewIDsExcludePrimary = ProductHelper.getImageViewIDsExcludePrimary;
+
+  ngOnInit(): void {
+    this.videoUrl = this.getImageCdnUrl(this.product, 'youTubeVideos', 'view1');
+    if (this.videoUrl) {
+      this.secureVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.videoUrl + '?enablejsapi=1&version=3&playerapiid=ytplayer'
+      );
+      this.secureVideoThumbnailUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.getVideoThumbnailUrl());
+    }
+  }
 
   /**
    * Set the active slide via index (used by the thumbnail indicator)
    * @param slideIndex The slide index number to set the active slide
    */
   setActiveSlide(slideIndex: number) {
-    this.activeSlide = slideIndex;
+    this.activeSlide = `${slideIndex}`;
+    this.ngCarousel.select(`${slideIndex}`);
+
+    // pause video
+    if (this.videoFrame) {
+      this.videoFrame.nativeElement.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    }
   }
 
   /**
@@ -41,6 +65,24 @@ export class CamfilProductImagesComponent {
    * @returns True if the given slide index is the active slide, false otherwise
    */
   isActiveSlide(slideIndex: number): boolean {
-    return this.activeSlide === slideIndex;
+    return this.activeSlide === `${slideIndex}`;
+  }
+
+  /**
+   * Returns the index of the video thumbnail
+   * @returns index
+   */
+  getVideoThumbnailIndex(): number {
+    return this.getImageViewIDsExcludePrimary(this.product, 'images').length + 2;
+  }
+
+  /**
+   * Returns the index of the video thumbnail
+   * @returns index
+   */
+  getVideoThumbnailUrl(): string {
+    var videoId = this.videoUrl.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+    var thumbnailUrl = '//img.youtube.com/vi/' + videoId + '/mqdefault.jpg';
+    return thumbnailUrl;
   }
 }
