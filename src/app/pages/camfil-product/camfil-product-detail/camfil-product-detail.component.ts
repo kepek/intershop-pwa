a1286e832d9f67c87f80a6133535d366794c499d
Pawel <pawel.butra@columbusglobal.com>
2021-02-03 09:42:34 +0100

    fix add to compare on pdp

--- src/app/pages/camfil-product/camfil-product-detail/camfil-product-detail.component.ts
+++ src/app/pages/camfil-product/camfil-product-detail/camfil-product-detail.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { startWith, take, takeUntil } from 'rxjs/operators';
 
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { CategoryView } from 'ish-core/models/category-view/category-view.model';
@@ -27,12 +27,16 @@ export class CamfilProductDetailComponent implements OnInit, OnDestroy {
   @Input() quantity: number;
   @Input() price: ProductPrices;
   @Input() variationOptions: VariationOptionGroup[];
-  @Input() isInCompareList: boolean;
   @Output() productToBasket = new EventEmitter<{ sku: string; quantity: number }>();
   @Output() productToCompare = new EventEmitter<string>();
   @Output() selectVariation = new EventEmitter<{ selection: VariationSelection; changedAttribute?: string }>();
   @Output() quantityChange = new EventEmitter<number>();
   @Output() compareToggle = new EventEmitter<void>();
+  @Input() productSku: string;
+  @Output() productSkuChange = new EventEmitter<string>();
+
+  isInCompareList$: Observable<boolean>;
+  private sku$ = new ReplaySubject<string>(1);
 
   constructor(private shoppingFacade: ShoppingFacade) {}
 
@@ -65,6 +69,11 @@ export class CamfilProductDetailComponent implements OnInit, OnDestroy {
           this.category = category;
         });
     }
+
+    this.productSkuChange.pipe(startWith(this.productSku), takeUntil(this.destroy$)).subscribe(this.sku$);
+    if (this.product?.sku) {
+      this.isInCompareList$ = this.shoppingFacade.inCompareProducts$(this.product.sku);
+    }
   }
 
   ngOnDestroy() {
@@ -80,7 +89,8 @@ export class CamfilProductDetailComponent implements OnInit, OnDestroy {
   }
 
   toggleCompare() {
-    this.compareToggle.emit();
+    // this.compareToggle.emit();
+    this.sku$.pipe(take(1), takeUntil(this.destroy$)).subscribe(sku => this.shoppingFacade.toggleProductCompare(sku));
   }
 
   addToCompare() {
