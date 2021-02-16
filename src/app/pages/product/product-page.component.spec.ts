627255dcc79d464f6c411098b62b79244f92a46c
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2021-02-16 15:01:46 +0100

    fix test

--- src/app/pages/product/product-page.component.spec.ts
+++ src/app/pages/product/product-page.component.spec.ts
@@ -84,7 +84,7 @@ describe('Product Page Component', () => {
 
     fixture.detectChanges();
 
-    expect(findAllCustomElements(element)).toEqual(['ish-loading', 'ish-recently-viewed']);
+    expect(findAllCustomElements(element)).toEqual(['ish-loading']);
   });
 
   it('should display product-detail when product is available', () => {
@@ -93,11 +93,7 @@ describe('Product Page Component', () => {
 
     fixture.detectChanges();
 
-    expect(findAllCustomElements(element)).toEqual([
-      'ish-product-detail',
-      'camfil-product-links',
-      'ish-recently-viewed',
-    ]);
+    expect(findAllCustomElements(element)).toEqual(['ish-product-detail', 'camfil-product-links']);
   });
 
   it('should redirect to product page when variation is selected', fakeAsync(() => {
