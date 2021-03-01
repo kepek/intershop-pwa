8aadb23dc0885d4a2e8a02eae95285c69db9e0f1
Philipp Sauer <PSauer@intershop.com>
2021-03-01 16:09:27 +0100

    Apply fix for display names of sorting keys to camfil custom component

--- src/app/shared/components/product/camfil-product-list-toolbar/camfil-product-list-toolbar.component.ts
+++ src/app/shared/components/product/camfil-product-list-toolbar/camfil-product-list-toolbar.component.ts
@@ -1,21 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChange,
-  SimpleChanges,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { isEqual } from 'lodash-es';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { FilterNavigation } from 'ish-core/models/filter-navigation/filter-navigation.model';
+import { SortableAttributesType } from 'ish-core/models/product-listing/product-listing.model';
 import { ViewType } from 'ish-core/models/viewtype/viewtype.types';
 import { URLFormParams, formParamsToString } from 'ish-core/utils/url-form-params';
 import { SelectOption } from 'ish-shared/forms/components/select/select.component';
@@ -26,11 +16,11 @@ import { SelectOption } from 'ish-shared/forms/components/select/select.componen
   styleUrls: ['./camfil-product-list-toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CamfilProductListToolbarComponent implements OnInit, OnChanges, OnDestroy {
+export class CamfilProductListToolbarComponent implements OnInit, OnChanges {
   @Input() itemCount: number;
   @Input() viewType: ViewType = 'simple';
   @Input() sortBy = 'default';
-  @Input() sortKeys: string[];
+  @Input() sortableAttributes: SortableAttributesType[];
   @Input() currentPage: number;
   @Input() pageIndices: number[];
   @Input() fragmentOnRouting: string;
@@ -42,49 +32,34 @@ export class CamfilProductListToolbarComponent implements OnInit, OnChanges, OnD
   sortOptions: SelectOption[] = [];
   filter$: Observable<FilterNavigation>;
 
-  private destroy$ = new Subject();
-
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private shoppingFacade: ShoppingFacade) {}
 
   ngOnInit() {
     this.filter$ = this.shoppingFacade.currentFilter$(this.showCategoryFilter);
-    this.sortDropdown.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(sorting => {
-      this.router.navigate([], {
-        relativeTo: this.activatedRoute,
-        queryParamsHandling: 'merge',
-        queryParams: this.isPaging ? { sorting, page: 1 } : { sorting },
-        fragment: this.fragmentOnRouting,
-      });
+  }
+
+  ngOnChanges() {
+    this.sortOptions = this.mapSortableAttributesToSelectOptions(this.sortableAttributes);
+  }
+
+  changeSortBy(target: EventTarget) {
+    // tslint:disable-next-line: no-string-literal
+    const sorting = target['value'];
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParamsHandling: 'merge',
+      queryParams: this.isPaging ? { sorting, page: 1 } : { sorting },
+      fragment: this.fragmentOnRouting,
     });
   }
 
-  ngOnChanges(c: SimpleChanges) {
-    if (c.sortKeys && !isEqual(c.sortKeys.currentValue, c.sortKeys.previousValue)) {
-      this.updateSortKeys(c.sortKeys);
-    }
-    this.updateSortBy(c.sortBy);
-  }
-
-  private updateSortBy(sortBy: SimpleChange) {
-    if (sortBy) {
-      this.sortDropdown.setValue(this.sortBy || undefined, { emitEvent: false });
-    }
-  }
-
-  private updateSortKeys(sortKeys: SimpleChange) {
-    if (sortKeys) {
-      this.sortOptions = this.mapSortKeysToSelectOptions(this.sortKeys);
-    }
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  private mapSortKeysToSelectOptions(sortKeys: string[]): SelectOption[] {
-    // TODO: probably it's good to map this in a selector, not here
-    return sortKeys.map(sk => ({ value: sk, label: sk }));
+  private mapSortableAttributesToSelectOptions(sortableAttributes: SortableAttributesType[] = []): SelectOption[] {
+    const options = sortableAttributes
+      .filter(x => !!x)
+      .map(sk => ({ value: sk.name, label: sk.displayName || sk.name }))
+      .sort((a, b) => a.label.localeCompare(b.label));
+    options.unshift({ value: 'default', label: undefined });
+    return options;
   }
 
   get simpleView() {
