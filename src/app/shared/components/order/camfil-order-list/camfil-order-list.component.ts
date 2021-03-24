c51ec272c01f2219a6f0849ccd0268c3cc1458eb
Philipp Sauer <PSauer@intershop.com>
2021-03-24 17:51:38 +0100

    CAM-411 As a User I want to be displayed how many orders the system display

--- src/app/shared/components/order/camfil-order-list/camfil-order-list.component.ts
+++ src/app/shared/components/order/camfil-order-list/camfil-order-list.component.ts
@@ -277,16 +277,22 @@ export class CamfilOrderListComponent implements OnInit, AfterViewInit, OnDestro
 
   showAllOrders() {
     this.showPreview = false;
-    this.previewSize = Number.MAX_VALUE;
+    this.previewSize = this.dataSource.filteredData.length;
   }
 
   isMaxTableLength() {
-    return this.dataSource.filteredData.length >= this.previewSize;
+    return this.dataSource.filteredData.length >= this.previewSize + 2; // 2 preview rows will be visible below "show more" overlay
   }
 
   updateFilter(filteredValues) {
     const filter = JSON.stringify(filteredValues);
     this.dataSource.filter = filter.trim();
     this.router.navigate([], { queryParams: { filter } });
+  }
+
+  countRows() {
+    return this.dataSource.filteredData.length < this.previewSize
+      ? this.dataSource.filteredData.length
+      : this.previewSize;
   }
 }
