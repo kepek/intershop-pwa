5dc6cc81271dda4d6e350881ee1732da3aa8d3f6
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2021-04-09 10:58:17 +0200

    fix - CAM-947 - CamCards is not displayed in the Add to CamCard lightbox

--- src/app/extensions/cam-cards/shared/select-cam-card-modal/select-cam-card-modal.component.ts
+++ src/app/extensions/cam-cards/shared/select-cam-card-modal/select-cam-card-modal.component.ts
@@ -82,7 +82,6 @@ export class SelectCamCardModalComponent implements OnInit, OnDestroy, OnChanges
 
   camCards: CamCard[];
 
-  camCards$: Observable<CamCard[]>;
   currentCamCard$: Observable<CamCard>;
   currentCamCard: CamCard;
 
