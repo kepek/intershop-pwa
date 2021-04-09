02fb10d71f37f23cf4700bfb62897fe00df13adc
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2021-04-09 12:34:08 +0200

    fix - CAM-947 - CamCards is not displayed in the Add to CamCard lightbox

--- src/app/extensions/cam-cards/shared/product-add-to-cam-card/product-add-to-cam-card.component.spec.ts
+++ src/app/extensions/cam-cards/shared/product-add-to-cam-card/product-add-to-cam-card.component.spec.ts
@@ -65,6 +65,7 @@ describe('Product Add To Cam Card Component', () => {
     component = fixture.componentInstance;
     element = fixture.nativeElement;
     when(camCardFacadeMock.camCard$).thenReturn(of(camCardDetails));
+    when(camCardFacadeMock.camCardsLoading$).thenReturn(of(false));
     component.product = { name: 'Test Product', sku: 'test sku' } as Product;
     component.quantity = 1;
   });
