dba0841615a889b816d21af23ac252488733e1b5
Przemyslaw Lupinski <przemyslaw.lupinski@istone.com>
2021-04-21 14:18:21 +0200

    CAM-927 - create url based on baseHref

--- src/app/core/pipes/make-href.pipe.ts
+++ src/app/core/pipes/make-href.pipe.ts
@@ -1,8 +1,6 @@
 import { Location } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { Channel } from 'ish-core/models/channel/channel.types';
-
 @Pipe({ name: 'makeHref', pure: false })
 export class MakeHrefPipe implements PipeTransform {
   transform(location: Location, urlParams: { [key: string]: string }): string {
@@ -15,20 +13,9 @@ export class MakeHrefPipe implements PipeTransform {
     // url without query params
     let newUrl = split[0];
 
-    // for change language only
-    const splitUrl = newUrl.split('/').filter(x => x);
-    const splitLocal = splitUrl[0]?.split('-') || [];
-
-    if (this.isChannelContextPath(splitUrl, splitLocal, urlParams?.value)) {
-      splitLocal[1] = urlParams.value;
-      splitUrl[0] = splitLocal.join('-');
-      return splitUrl.join('/') + (split.length > 1 ? `?${split[1]}` : '');
-    }
-
     // add supplied url params
     if (urlParams) {
       newUrl += Object.keys(urlParams)
-        .filter(k => k !== 'value')
         .map(k => `;${k}=${urlParams[k]}`)
         .join('');
     }
@@ -39,14 +26,5 @@ export class MakeHrefPipe implements PipeTransform {
     }
 
     return newUrl;
-  }
-
-  isChannelContextPath(url: string[], local: string[], value: string) {
-    return (
-      url[0]?.length === 5 && // ex.: sv-se
-      local.length === 2 && // ex.: ['sv', 'se']
-      Object.keys(Channel).includes(local[1].toLocaleUpperCase()) &&
-      value
-    );
   }
 }
--- src/app/shell/header/camfil-language-switch/camfil-language-switch.component.ts
+++ src/app/shell/header/camfil-language-switch/camfil-language-switch.component.ts
@@ -1,7 +1,8 @@
-import { Location } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { APP_BASE_HREF, Location } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { filter, take } from 'rxjs/operators';
 
 import { AppFacade } from 'ish-core/facades/app.facade';
 import { Channel } from 'ish-core/models/channel/channel.types';
@@ -27,7 +28,12 @@ export class CamfilLanguageSwitchComponent implements OnInit {
   availableLocales$: Observable<Locale[]>;
   availableLocales: Locale[];
 
-  constructor(private appFacade: AppFacade, public location: Location) {}
+  constructor(
+    private appFacade: AppFacade,
+    public location: Location,
+    private router: Router,
+    @Inject(APP_BASE_HREF) private baseHref: string
+  ) {}
 
   ngOnInit() {
     this.locale$ = this.appFacade.currentLocale$;
@@ -38,11 +44,36 @@ export class CamfilLanguageSwitchComponent implements OnInit {
           .find(([, val]) => val === channel)[0]
           .toLowerCase();
         this.availableLocales = availableLocales.filter(loc => [channelCode, 'gb'].includes(loc.value));
+
+        // TODO: tmp part
+        this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd)).subscribe(event => {
+          console.log(event.url, 'event.url');
+          console.log(this.router.url, 'url');
+          console.log(this.baseHref, 'baseHref');
+          console.log(this.location.path(), 'location');
+        });
       });
     });
   }
 
   toggleLevel(val: boolean) {
     this.isClosedLangList.emit(val);
+  }
+
+  getLanguageSwitchUrl(value: string) {
+    const baseHrefArr = this.baseHref.split('/').filter(x => x);
+    const locals = baseHrefArr[0]?.split('-');
+
+    if (
+      baseHrefArr[0]?.length === 5 && // ex.: sv-se
+      locals.length === 2 && // ex.: ['sv', 'se']
+      Object.keys(Channel).includes(locals[1].toLocaleUpperCase())
+    ) {
+      locals[1] = value;
+      baseHrefArr[0] = locals.join('-');
+
+      return `/${baseHrefArr.join('/')}${this.location.path()}`;
+    }
+    return false;
   }
 }
