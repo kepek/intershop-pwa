b21465e1f951aebba6767ae4802fbc7ad93b7a22
Philipp Sauer <PSauer@intershop.com>
2021-06-02 15:51:53 +0200

    CAM-47 Reorder imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { isPlatformBrowser } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
-
-import { AppFacade } from 'ish-core/facades/app.facade';
-import { DeviceType } from 'ish-core/models/viewtype/viewtype.types';
-import { CHANNEL_CONFIGURATION } from 'ish-core/configurations/injection-keys';
-import { ChannelConfiguration } from 'ish-core/models/channel-configuration/channel-configuration.model';
-import { CookiesService } from 'ish-core/utils/cookies/cookies.service';
-import { select, Store } from '@ngrx/store';
-import { getCamfilChannel } from 'ish-core/store/core/configuration';
-import { whenTruthy } from 'ish-core/utils/operators';
 import { take } from 'rxjs/operators';
+
+import { CHANNEL_CONFIGURATION } from 'ish-core/configurations/injection-keys';
+import { AppFacade } from 'ish-core/facades/app.facade';
 import { FeatureToggleService } from 'ish-core/feature-toggle.module';
+import { ChannelConfiguration } from 'ish-core/models/channel-configuration/channel-configuration.model';
+import { DeviceType } from 'ish-core/models/viewtype/viewtype.types';
+import { getCamfilChannel } from 'ish-core/store/core/configuration';
+import { CookiesService } from 'ish-core/utils/cookies/cookies.service';
+import { whenTruthy } from 'ish-core/utils/operators';
+
 /**
  * The App Component provides the application frame for the single page application.
  * In addition to the page structure (header, main section, footer)
