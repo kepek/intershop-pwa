903e0f4978d9712288f9bb4537cdb650736b0f93
Pawel <pawel.butra@columbusglobal.com>
2021-06-08 12:40:28 +0200

    npm run lint

--- src/app/pages/camfil-checkout/camfil-checkout-list/camfil-date-adapter.ts
+++ src/app/pages/camfil-checkout/camfil-checkout-list/camfil-date-adapter.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
+
 @Injectable()
 export class CamfilDateAdapter extends NativeDateAdapter {
   getFirstDayOfWeek(): number {
