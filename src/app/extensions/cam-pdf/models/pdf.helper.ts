fbd193ba8d69f9cabe70d73682c728a434e08975
Michal Kechner <michal.kechner@columbusglobal.com>
2021-06-11 22:30:17 +0200

    npm run lint

--- src/app/extensions/cam-pdf/models/pdf.helper.ts
+++ src/app/extensions/cam-pdf/models/pdf.helper.ts
@@ -88,9 +88,9 @@ export class PdfHelper {
         fontSize: 10,
         margin: [0, 10, 0, 30],
       },
-      content:{
+      content: {
         margin: [0, 10, 0, 10],
-      }
+      },
     };
   }
 }
