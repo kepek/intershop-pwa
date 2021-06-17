8372a2af610cf10f1bae9434e5a11816daf50aba
Pawel <pawel.butra@columbusglobal.com>
2021-06-17 15:59:25 +0200

    sort value and missing icon

--- src/app/shared/components/product/camfil-product-list-toolbar/camfil-product-list-toolbar.component.ts
+++ src/app/shared/components/product/camfil-product-list-toolbar/camfil-product-list-toolbar.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { FilterNavigation } from 'ish-core/models/filter-navigation/filter-navigation.model';
@@ -9,6 +9,8 @@ import { SortableAttributesType } from 'ish-core/models/product-listing/product-
 import { ViewType } from 'ish-core/models/viewtype/viewtype.types';
 import { URLFormParams, formParamsToString } from 'ish-core/utils/url-form-params';
 import { SelectOption } from 'ish-shared/forms/components/select/select.component';
+import { whenTruthy } from 'ish-core/utils/operators';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'camfil-product-list-toolbar',
@@ -16,7 +18,7 @@ import { SelectOption } from 'ish-shared/forms/components/select/select.componen
   styleUrls: ['./camfil-product-list-toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CamfilProductListToolbarComponent implements OnInit, OnChanges {
+export class CamfilProductListToolbarComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() itemCount: number;
   @Input() viewType: ViewType = 'simple';
   @Input() sortBy = 'default';
@@ -27,19 +29,37 @@ export class CamfilProductListToolbarComponent implements OnInit, OnChanges {
   @Input() isPaging = false;
   @Input() categoryName: string;
   @Input() showCategoryFilter = false;
-
+  sortingParam: string;
   sortDropdown = new FormControl('');
   sortOptions: SelectOption[] = [];
   filter$: Observable<FilterNavigation>;
+
+  private destroy$ = new Subject<void>();
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private shoppingFacade: ShoppingFacade) {}
 
   ngOnInit() {
     this.filter$ = this.shoppingFacade.currentFilter$(this.showCategoryFilter);
+    this.activatedRoute.queryParamMap.pipe(whenTruthy(), takeUntil(this.destroy$)).subscribe(params => {
+      if (params) {
+        this.sortingParam = params.get('sorting');
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    if (this.sortingParam) {
+      this.sortDropdown.setValue(this.sortingParam);
+    }
   }
 
   ngOnChanges() {
     this.sortOptions = this.mapSortableAttributesToSelectOptions(this.sortableAttributes);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   changeSortBy(target: EventTarget) {
