340819dafb83bde0c018084293bcb33d11a51903
Michal Kechner <michal.kechner@columbusglobal.com>
2021-06-24 09:03:50 +0200

    test: fix broken tests

--- src/app/shared/components/basket/camfil-basket-validation-results/camfil-basket-validation-results.component.spec.ts
+++ src/app/shared/components/basket/camfil-basket-validation-results/camfil-basket-validation-results.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MockComponent, MockPipe } from 'ng-mocks';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
+
+import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
+import { CamfilSlugifyPipe } from 'ish-core/pipes/camfil-slugify.pipe';
+import { BasketValidationItemsComponent } from 'ish-shared/components/basket/basket-validation-items/basket-validation-items.component';
+import { BasketValidationProductsComponent } from 'ish-shared/components/basket/basket-validation-products/basket-validation-products.component';
 
 import { CamfilBasketValidationResultsComponent } from './camfil-basket-validation-results.component';
 
@@ -6,10 +15,21 @@ describe('Camfil Basket Validation Results Component', () => {
   let component: CamfilBasketValidationResultsComponent;
   let fixture: ComponentFixture<CamfilBasketValidationResultsComponent>;
   let element: HTMLElement;
+  let checkoutFacadeMock: CheckoutFacade;
 
   beforeEach(async () => {
+    checkoutFacadeMock = mock(CheckoutFacade);
+    when(checkoutFacadeMock.basketValidationResults$).thenReturn(of(undefined));
+
     await TestBed.configureTestingModule({
-      declarations: [CamfilBasketValidationResultsComponent],
+      declarations: [
+        CamfilBasketValidationResultsComponent,
+        MockComponent(BasketValidationItemsComponent),
+        MockComponent(BasketValidationProductsComponent),
+        MockPipe(CamfilSlugifyPipe),
+      ],
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: CheckoutFacade, useFactory: () => instance(checkoutFacadeMock) }],
     }).compileComponents();
   });
 
