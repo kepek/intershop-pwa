8da24f2208b96d2f31a03f0093ef64e38f77366d
psauer-ish <p.sauer@intershop.de>
2021-07-06 11:55:23 +0200

    CAM-1324 lint

--- src/app/pages/cookies/cookies-page.module.ts
+++ src/app/pages/cookies/cookies-page.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Routes } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { CamfilCookiesModalComponent } from './camfil-cookies-modal/camfil-cookies-modal.component';
-import { CookiesPageGuard } from './cookies-page.guard';
-import { CookiesModalComponent } from './cookies-modal/cookies-modal.component';
-import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from 'ish-shared/shared.module';
+
+import { CamfilCookiesModalComponent } from './camfil-cookies-modal/camfil-cookies-modal.component';
+import { CookiesModalComponent } from './cookies-modal/cookies-modal.component';
+import { CookiesPageGuard } from './cookies-page.guard';
 
 const cookiesPageRoutes: Routes = [
   {
@@ -24,7 +25,7 @@ const cookiesPageRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [SharedModule, CommonModule, RouterModule.forChild(cookiesPageRoutes), TranslateModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule.forChild(cookiesPageRoutes), SharedModule, TranslateModule],
   providers: [CookiesPageGuard],
   declarations: [CamfilCookiesModalComponent, CookiesModalComponent],
 })
