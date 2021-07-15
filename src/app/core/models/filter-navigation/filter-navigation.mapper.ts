6df34ac3f6ba621c150119fd6d4809e4ffff84f4
Michal Kechner <michal.kechner@columbusglobal.com>
2021-07-15 14:51:50 +0200

    fix: make sure encoded filters are skipped from the list

--- src/app/core/models/filter-navigation/filter-navigation.mapper.ts
+++ src/app/core/models/filter-navigation/filter-navigation.mapper.ts
@@ -82,31 +82,62 @@ export class FilterNavigationMapper {
     return filterEntry.mappedValue;
   }
 
+  private decodeFacetName(name: string) {
+    try {
+      return decodeURIComponent(name);
+    } catch (err) {
+      return name;
+    }
+  }
+
+  private isSelectedFacetBroken(facet: FacetData) {
+    return facet.selected && this.decodeFacetName(facet?.name) !== facet?.name;
+  }
+
+  private healBrokenFilterEntries(filterEntries: FacetData[]) {
+    // tslint:disable-next-line:variable-name
+    return filterEntries.reduce((acc, facet, _index, facets) => {
+      if (this.isSelectedFacetBroken(facet)) {
+        const healthyFacet = facets.find(f => f.name === this.decodeFacetName(facet.name));
+
+        if (healthyFacet) {
+          healthyFacet.selected = facet.selected;
+          healthyFacet.link = facet.link;
+        }
+      } else {
+        acc.push(facet);
+      }
+
+      return acc;
+    }, []);
+  }
+
   private mapFacetData(filterData: FilterData) {
-    return filterData.filterEntries
-      ? filterData.filterEntries.reduce((acc, facet) => {
-          const category = facet.link.uri.includes('/categories/')
-            ? [facet.link.uri.split('/productfilters')[0].split('/categories/')[1]]
-            : undefined;
-          if (facet.name !== 'Show all') {
-            acc.push({
-              name: facet.name,
-              count: facet.count,
-              selected: facet.selected,
-              displayName: FilterNavigationMapper.parseFilterDisplayName(facet.displayValue) || undefined,
-              searchParameter: {
-                ...stringToFormParams(facet.link.uri.split('?')[1] || ''),
-                category,
-              },
-              level: facet.level || 0,
-              mappedValue: this.parseFilterValue(facet),
-              mappedType: facet.mappedType || undefined,
-            });
-          } else {
-            console.warn(`Limiting filters is not supported. Set limit to -1 in the BackOffice (${filterData.name})`);
-          }
-          return acc;
-        }, [])
-      : [];
+    // Due to CAM-1293 we have to make sure that broken (encoded) filters are skipped from the list.
+    const filterEntries = filterData?.filterEntries ? this.healBrokenFilterEntries(filterData.filterEntries) : [];
+
+    return filterEntries.reduce((acc, facet) => {
+      const category = facet.link.uri.includes('/categories/')
+        ? [facet.link.uri.split('/productfilters')[0].split('/categories/')[1]]
+        : undefined;
+      if (facet.name !== 'Show all') {
+        acc.push({
+          name: facet.name,
+          count: facet.count,
+          selected: facet.selected,
+          displayName: FilterNavigationMapper.parseFilterDisplayName(facet.displayValue) || undefined,
+          searchParameter: {
+            ...stringToFormParams(facet.link.uri.split('?')[1] || ''),
+            category,
+          },
+          level: facet.level || 0,
+          mappedValue: this.parseFilterValue(facet),
+          mappedType: facet.mappedType || undefined,
+        });
+      } else {
+        console.warn(`Limiting filters is not supported. Set limit to -1 in the BackOffice (${filterData.name})`);
+      }
+      return acc;
+    }, []);
   }
 }
