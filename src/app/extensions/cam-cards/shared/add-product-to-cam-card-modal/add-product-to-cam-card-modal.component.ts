9b8df501649f81299e3ef27a1a868d6c36563611
Pawel <pawel.butra@columbusglobal.com>
2021-08-24 11:25:44 +0200

    focus new section fix

--- src/app/extensions/cam-cards/shared/add-product-to-cam-card-modal/add-product-to-cam-card-modal.component.ts
+++ src/app/extensions/cam-cards/shared/add-product-to-cam-card-modal/add-product-to-cam-card-modal.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
   OnChanges,
@@ -49,6 +50,7 @@ import { CreateProductCamCardModalComponent } from './create-product-cam-card-mo
 export class AddProductToCamCardModalComponent implements OnInit, OnDestroy, OnChanges {
   @Input() product: Product;
   @Input() quantity: number;
+  @ViewChild('newSubCamCardSegment') newSubCamCardInput: ElementRef;
 
   /**
    * changes the some logic and the translations keys between add or move a product (default: 'add')
