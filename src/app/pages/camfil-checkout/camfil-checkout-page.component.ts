0124681efa786e666ab3f10d9e6d09a91538aa30
Michal Kechner <michal.kechner@columbusglobal.com>
2021-10-16 14:25:25 +0200

    fix: add checkout guard

--- src/app/pages/camfil-checkout/camfil-checkout-page.component.ts
+++ src/app/pages/camfil-checkout/camfil-checkout-page.component.ts
@@ -12,7 +12,8 @@ import { BasketView } from 'ish-core/models/basket/basket.model';
 import { Bucket } from 'ish-core/models/basket/bucket.model';
 import { Order } from 'ish-core/models/order/order.model';
 import { whenTruthy } from 'ish-core/utils/operators';
-import { AccountFacade } from 'ish-core/facades/account.facade';
+
+import { CamCardsFacade } from '../../extensions/cam-cards/facades/cam-cards.facade';
 
 import { CamCardsFacade } from '../../extensions/cam-cards/facades/cam-cards.facade';
 
