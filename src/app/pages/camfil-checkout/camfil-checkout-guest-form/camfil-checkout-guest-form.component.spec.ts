9158a6cddb79849abfe6773a18080791adf1f4ef
PawelButra <pawel.butra@columbusglobal.com>
2021-11-08 13:43:04 +0100

    include error component in tests

--- src/app/pages/camfil-checkout/camfil-checkout-guest-form/camfil-checkout-guest-form.component.spec.ts
+++ src/app/pages/camfil-checkout/camfil-checkout-guest-form/camfil-checkout-guest-form.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
 import { instance, mock } from 'ts-mockito';
 
 import { CamfilMaxLengthAttributeCreateDirective } from 'ish-core/directives/camfil-max-length-attribute-create.directive';
 import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
+import { CamfilErrorComponent } from 'ish-shared/components/common/camfil-error/camfil-error.component';
 
 import { CamConfigurationFacade } from '../../../extensions/cam-configuration/facades/cam-configuration.facade';
 
@@ -19,7 +21,11 @@ describe('Camfil Checkout Guest Form Component', () => {
     camConfigurationFacade = mock(CamConfigurationFacade);
     checkoutFacadeMock = mock(CheckoutFacade);
     await TestBed.configureTestingModule({
-      declarations: [CamfilCheckoutGuestFormComponent, CamfilMaxLengthAttributeCreateDirective],
+      declarations: [
+        CamfilCheckoutGuestFormComponent,
+        CamfilMaxLengthAttributeCreateDirective,
+        MockComponent(CamfilErrorComponent),
+      ],
       providers: [
         { provide: CamConfigurationFacade, useFactory: () => instance(camConfigurationFacade) },
         { provide: CheckoutFacade, useFactory: () => instance(checkoutFacadeMock) },
