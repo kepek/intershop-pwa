f16aa20c38a22e76e0916742c895d60e3dfe91bc
Przemysław Łupiński (PRL.PL) <przemyslaw.lupinski@columbusglobal.com>
2021-11-25 15:04:41 +0100

    CAM-1792 - Please remove a validation message for anonymous user

--- src/app/shared/components/common/camfil-error/camfil-error.component.ts
+++ src/app/shared/components/common/camfil-error/camfil-error.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 interface ErrorValidator {
   error: string;
@@ -13,12 +13,21 @@ interface ErrorValidator {
   styleUrls: ['./camfil-error.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CamfilErrorComponent {
+export class CamfilErrorComponent implements OnChanges {
   @Input() errorValidators: ErrorValidator[];
   @Input() touched: boolean;
   @Input() errors: {};
 
+  processedErrorValidators: ErrorValidator[];
+
   getMessageVariables(validator: ErrorValidator) {
     return validator.messageVariables?.reduce((acc, item, index) => ({ ...acc, [index]: item }), {}) || {};
+  }
+
+  ngOnChanges(c: SimpleChanges) {
+    const currentValue = c.errors?.currentValue;
+    if (currentValue && Object.keys(currentValue).length > 1 && 'required' in currentValue) {
+      this.processedErrorValidators = this.errorValidators.filter(err => err.error !== 'required');
+    }
   }
 }
