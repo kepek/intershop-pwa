8715593aa3829d070db6997d1f58c279e4dc08ee
Michal Kechner <michal.kechner@columbusglobal.com>
2022-05-13 10:59:54 +0200

    hotfix: enable show all docs

--- projects/camfil-pwa/src/app/settings/SE.ts
+++ projects/camfil-pwa/src/app/settings/SE.ts
@@ -14,6 +14,7 @@ const camfilConfiguration: CamfilChannelConfiguration = {
   showTotalWithoutTaxInBasketSummary: false,
   hidePricesCamCards: true,
   filterDocsByLanguage: true,
+  showAllDocsType: true,
 };
 
 export default camfilConfiguration;
