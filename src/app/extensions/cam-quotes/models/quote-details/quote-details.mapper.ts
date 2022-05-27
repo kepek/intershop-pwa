a48dd6b7b115fc18d90edb4f7b8bde16df148795
Alfonso Chaparro <alfonso.chaparro.m@gmail.com>
2022-05-27 15:47:35 +0200

    quote items single price calculated if not received from api

--- src/app/extensions/cam-quotes/models/quote-details/quote-details.mapper.ts
+++ src/app/extensions/cam-quotes/models/quote-details/quote-details.mapper.ts
@@ -36,7 +36,15 @@ export class QuoteDetailsMapper {
         editable: data.editable,
         submitted: data.submitted,
         total: data.total,
-        items: [...data.items],
+        items: [...data.items].map(item => {
+          if (!item.singlePrice) {
+            item.singlePrice = {
+              ...item.totalPrice,
+              value: item.totalPrice.value / item.quantity.value,
+            };
+          }
+          return item;
+        }),
         deliveryAddress: data.deliveryAddress ? AddressMapper.fromData(data.deliveryAddress) : undefined,
         quotationReference: data.quotationReference,
         validToDate: data.validToDate,
