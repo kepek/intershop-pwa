216b21af16e9e1560b62d3568f0746882f892f70
Michal Kechner <michal.kechner@columbusglobal.com>
2022-06-03 13:32:45 +0200

    fix: remove measurment validation check when adding cc from edit page

--- src/app/extensions/cam-cards/pages/account-cam-card-detail/account-cam-card-detail-list/account-cam-card-detail-list.component.ts
+++ src/app/extensions/cam-cards/pages/account-cam-card-detail/account-cam-card-detail-list/account-cam-card-detail-list.component.ts
@@ -354,12 +354,12 @@ export class AccountCamCardDetailListComponent implements OnInit, OnChanges, OnD
   addToCart() {
     const ccId = this.camCard.id;
     const products = this.camCard.camCardItems
-      ?.filter(cc => cc.measurement.valid && cc.product.available)
+      ?.filter(cc => cc.product.available)
       .map(item => this.prepereItemToAdd(this.camCard, item));
 
     this.camCard.subCamCards?.forEach(sub => {
       sub.camCardItems
-        ?.filter(sc => sc.measurement.valid && sc.product.available)
+        ?.filter(sc => sc.product.available)
         .forEach(item => {
           products.push(this.prepereItemToAdd(sub, item));
         });
