6111cb9db2680fe089defa7f8b128683cbba363c
PawelButra <pawel.butra@columbusglobal.com>
2022-06-09 10:33:13 +0200

    user comment to info and remove buttons on buyer page

--- src/app/extensions/cam-requisition-management/pages/requisition-detail/requisition-detail-page.component.ts
+++ src/app/extensions/cam-requisition-management/pages/requisition-detail/requisition-detail-page.component.ts
@@ -76,6 +76,9 @@ export class RequisitionDetailPageComponent implements OnInit, OnDestroy {
       }
     });
     this.lineItems$ = this.context.select('lineItems');
+    this.view$.pipe(takeUntil(this.destroy$)).subscribe(val => {
+      console.log('Value', val);
+    });
   }
 
   approveRequisition() {
