0da6675acbfb8db170ab4dbe29ae6d4929bb9732
Alfonso Chaparro <alfonso.chaparro.m@gmail.com>
2022-06-15 17:03:32 +0200

    stay at checkout after quotation created

--- src/app/pages/camfil-checkout-onestep/camfil-checkout-onestep-page.component.ts
+++ src/app/pages/camfil-checkout-onestep/camfil-checkout-onestep-page.component.ts
@@ -99,7 +99,8 @@ export class CamfilCheckoutOnestepPageComponent implements OnInit, OnDestroy {
 
     this.isEditable$ = this.submittedBasket$.pipe(
       startWith(false),
-      map(submittedBasket => !submittedBasket)
+      withLatestFrom(this.checkoutFacade.selectedOrder$),
+      map(([submittedBasket, order]) => !submittedBasket || order.statusCode === 'RFQ')
     );
 
     this.isEmpty$ = this.allBuckets$.pipe(map(allBuckets => allBuckets?.length === 0));
