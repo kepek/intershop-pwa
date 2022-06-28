fed722632300d8f68811db9dbc9ba24b714a2f36
PawelButra <pawel.butra@columbusglobal.com>
2022-06-28 13:57:26 +0200

    fix tracking config

--- src/app/extensions/tracking/store/tracking-config/tracking-config.effects.ts
+++ src/app/extensions/tracking/store/tracking-config/tracking-config.effects.ts
@@ -1,3 +1,4 @@
+import { isPlatformServer } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Actions, createEffect } from '@ngrx/effects';
@@ -42,7 +43,7 @@ export class TrackingConfigEffects {
 
   setGTMToken$ = createEffect(() =>
     this.actions$.pipe(
-      takeWhile(() => this.featureToggleService.enabled('tracking')),
+      takeWhile(() => isPlatformServer(this.platformId) && this.featureToggleService.enabled('tracking')),
       take(1),
       withLatestFrom(this.stateProperties.getStateOrEnvOrDefault<string>('GTM_TOKEN', 'gtmToken')),
       map(([, gtmToken]) => gtmToken),
