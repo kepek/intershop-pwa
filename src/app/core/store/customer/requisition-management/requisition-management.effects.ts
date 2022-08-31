0befa742994a4493872ae48cd2f56df8e796c173
Michal Kechner <michal.kechner@columbusglobal.com>
2022-08-31 15:18:17 +0200

    fix: disable requisition breadcrumb effects for the customer due to errors in runtime

--- src/app/core/store/customer/requisition-management/requisition-management.effects.ts
+++ src/app/core/store/customer/requisition-management/requisition-management.effects.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
-import { createEffect } from '@ngrx/effects';
-import { RequisitionManagementBreadcrumbService } from 'requisition-management';
-import { map } from 'rxjs/operators';
-
-import { setBreadcrumbData } from 'ish-core/store/core/viewconf';
 
 @Injectable()
 export class RequisitionManagementEffects {
-  constructor(private requisitionManagementBreadcrumbService: RequisitionManagementBreadcrumbService) {}
-
-  setRequisitionManagementBreadcrumb$ = createEffect(() =>
-    this.requisitionManagementBreadcrumbService
-      .breadcrumb$('/account/requisitions')
-      .pipe(map(breadcrumbData => setBreadcrumbData({ breadcrumbData })))
-  );
+  // TODO (extMlk): Disable requisition breadcrumb service due to errors in the runtime. Refactor/Override in Extension;
+  // constructor(private requisitionManagementBreadcrumbService: RequisitionManagementBreadcrumbService) {}
+  // setRequisitionManagementBreadcrumb$ = createEffect(() =>
+  //   this.requisitionManagementBreadcrumbService
+  //     .breadcrumb$('/account/requisitions')
+  //     .pipe(map(breadcrumbData => setBreadcrumbData({ breadcrumbData })))
+  // );
 }
